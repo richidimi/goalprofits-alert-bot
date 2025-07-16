e6a643a38f89cd0f9693a1648e50026764d4036c
richidimi <riccardodimitrio@libero.it>
2025-07-16 18:00:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,5 +26,9 @@ def webhook():
 def ping():
     return "🟢 Bot attivo", 200
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
